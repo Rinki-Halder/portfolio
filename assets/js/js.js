b53139c6a60a73781db65b982557277a87a4a43e
Rinki-Halder <rinkih81@gmail.com>
2024-02-08 20:29:51 +0530

    landing page

---
+++ assets/js/js.js
@@ -0,0 +1,73 @@
+//Counter
+$(document).ready(function(){
+  $('.counter-value').each(function(){
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      },{
+          duration: 10000,
+          easing: 'swing',
+          step: function (now){
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
+});
+//Counter
+
+
+$('.navTrigger').click(function () {
+  $(this).toggleClass('active');
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
+
+});
+
+//Testimonial slide
+$(document).ready(function() {
+  $('.testimonial-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 1000,
+      speed: 600,
+      draggable: true,
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+      responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+          {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+          }
+      ]
+  });
+}); 
+//Testimonial slide
+
+//Page Up
+var btn = $('#pageup');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
